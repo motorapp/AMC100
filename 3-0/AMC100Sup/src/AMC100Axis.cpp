dd633a7502ea464434de9d58cd340001b99d4019
motorapp <padmoz@tpg.com.au>
2022-10-13 23:14:17 +1100

    Update AMC100Axis.cpp
    Update contact details

--- 3-0/AMC100Sup/src/AMC100Axis.cpp
+++ 3-0/AMC100Sup/src/AMC100Axis.cpp
@@ -13,8 +13,8 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 // Contact details:
-// cliftm@ansto.gov.au
-// 800 Blackburn Road, Clayton, Victoria 3168, Australia.
+// Mark Clift
+// padmoz@tpg.com.au
 //
 
 #include <stdio.h>
